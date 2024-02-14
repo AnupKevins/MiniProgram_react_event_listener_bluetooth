79ca5cf62925b2e5f132fc121722b4354355d0c9
Anup <anup.sahu@thoughtworks.com>
2024-02-14 14:19:53 +0530

    add camera event

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
+import backButton from './backButton.png';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+function handleClickOpenCamera() {
+  window.webkit.messageHandlers.iOS.postMessage("iOS_OpenCamera");
+}
+
+function handleClickOpenPhotos() {
+  window.webkit.messageHandlers.iOS.postMessage("iOS_OpenPhotos");
+}
 
 function handleClickBluetooth() {
   window.webkit.messageHandlers.iOS.postMessage("iOS_Bluetooth");
@@ -19,11 +27,20 @@ function handleClickNetwork() {
   window.webkit.messageHandlers.iOS.postMessage("iOS_Network")
 }
 
+function handleClickBack() {
+  window.webkit.messageHandlers.iOS.postMessage("iOS_Back");
+}
+
 function App() {
 
   const [hardwareList, setHardwareList] = useState([]);
+  const [imageData, setImageData] = useState(null);
 
   window.event = new Event('MyEventType');
+
+  window.displayImageFromNative = (imageData) => {
+    setImageData(imageData);
+  };
 
   window.handleBluetoothData = function(data){
     setHardwareList(data.split('|'));
@@ -51,11 +68,44 @@ function App() {
     <li key={item.toString()}>
       {item.toString()}
     </li>
-  );  
+  ); 
+  
+  useEffect(() => {
+    // Define the displayImageFromNative function
+    window.handleImageData = (imageData) => {
+      const imgElement = document.createElement('img');
+      imgElement.src = `data:image/jpeg;base64,${imageData}`;
+      document.body.appendChild(imgElement);
+    
+    };
+  }, []);
+
   return (
     <div className="App">
+    <div className="page-header">
+    <button onClick={handleClickBack} className="back-button">
+      <img
+        src={backButton} // Replace with the path to your arrow image
+        alt="Back"
+        style={{ width: '25px'}} // Adjust styles as needed
+      />
+    </button>
+    <header className="page-title">Mini-Programs</header>    
+    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Camera
+        </p>
+        <button onClick={handleClickOpenCamera}>
+          Open Camera
+        </button>
+        <p>
+          Gallery
+        </p>
+        <button onClick={handleClickOpenPhotos}>
+          Open Photos
+        </button>
         <p>
           Bluetooth
         </p>
@@ -80,8 +130,17 @@ function App() {
         <button onClick={handleClickNetwork}>
           Get Network Information
         </button>
-        <ul>{listItems}</ul>
+        <div>{listItems}</div>
       </header>
+      <div className="custom-image-container">
+          {imageData && 
+          <img 
+            // eslint-disable-next-line no-template-curly-in-string
+            src='data:image/jpeg;base64,${imageData}' 
+            alt='' 
+            className="custom-image"
+          />}
+        </div>
     </div>
   );
 }
